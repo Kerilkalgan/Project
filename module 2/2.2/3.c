69835846e2f43078fb6d54f1a4c9c5192f9a4e47
Kerilkalgan <kerilkalgan@github.com>
2016-03-25 23:32:26 +0300

    25/03

--- module 2/2.2/3.c
+++ module 2/2.2/3.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+
 int main(void) 
 {
 	char x[254];
 	printf("Введите строку у которой вы хотите узнать размер в байтах\n");
 	scanf("%s",x);
-	printf("В данной строчке %d байт(а)",strlen (x));
+	printf("В данной строчке %d байт(а)",strlen(x));
 	return 0;
 
 }
