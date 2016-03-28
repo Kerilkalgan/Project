5d3cc372a2f7ee120a7426f6e91dc24bc665f12d
Kerilkalgan <kerilkalgan@github.com>
2016-03-28 23:25:29 +0300

    28/03

--- module 2/2.2/8.c
+++ module 2/2.2/8.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 int main(void) 
 {
-	float v;
-	float a;
-	float sinx;
-	int g;
-	float t;
-	float x;
-	float Pi;
+	float v,min,hour,a,sinx,t,x,Pi,g;
+    g=9.8;//Ускорение свободного падения в м/с^2.
 	Pi=3.1415926539;
-	g=10; //Ускорение свободного падения в м/с^2.
 	printf("Программа вычисляет время полета брошеного камня под некоторым углом к горизонту\n"); 
 	printf("Введите начальную скорость (в м/с) = ");
 	scanf("%f",&v);
@@ -18,7 +12,9 @@ int main(void)
 	x=a*Pi/180;
 	sinx=x-x*x*x/6+x*x*x*x*x/120; //Разложение функции sin в ряд Тейлора (до трёх слагаемых).
 	t=2*v*sinx/g;
-	printf("Время полета = %f секунд \n",t);
+	min=t/60;
+	hour=min/60;
+	printf("Время полета = %f сек. = %f мин. = %f час. \n",t,min,hour);
     return 0;
 
     
