e37c85c3343e7d23faee5a46344bfcc41593355e
Kerilkalgan <kerilkalgan@github.com>
2016-03-31 00:37:01 +0300

    31/03

---
+++ module 2/2.2/10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Данная программа выводит количество байт,которые отводятся под каждый тип данных\n");
+	printf("Тип               Размер в байтах\n");
+	printf("char                     1\n");
+	printf("unsigned char            1\n");
+	printf("signed char              1\n");
+	printf("int                   2 или 4\n");
+	printf("unsigned int          2 или 4\n");
+	printf("signet int            2 или 4\n");
+	printf("short int                2\n");
+	printf("unsigned short int       2\n");
+	printf("signed short int         2\n");
+	printf("long int                 4\n");
+	printf("long long int            8\n");
+	printf("signed long int          4\n");
+	printf("unsigned long int        4\n");
+	printf("unsigned long long int   8\n");
+	printf("float                    4\n");
+	printf("double                   8\n");
+	printf("long double             10\n");
+	return 0;
+}
