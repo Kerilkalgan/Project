b3dbe90e92e2f1e718808857c6de762b84240067
Kerilkalgan <kerilkalgan@github.com>
2016-03-31 21:50:28 +0300

    31/03

---
+++ module 2/2.3/8.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+int main (void) 
+{
+	float x,y;
+	char a;
+	printf("Калькулятор.\n");
+	printf("\n   Здесь вы можете реализовать такие команды для двух чисел как: +,-,*,/,^.\n");
+	printf("   Введите своё выражение (например 7+9,8^16,...): ");
+	scanf("%f %c %f", &x, &a, &y);
+  
+    if ( a == '+')
+    {
+		printf("Ответ:%f",x+y);
+	}
+	if ( a == '-')
+	{
+		printf("Ответ:%f",x-y);
+	}
+    if ( a == '*')
+    {
+		printf("Ответ:%f",x*y);
+	}
+    if ( a == '/')
+    
+    {
+		if ( y== 0)
+		{
+			printf("Ошибка!!! На ноль делить нельзя\n");
+	}
+		else
+		{ 
+			printf("Ответ:%f",x/y);
+		}
+	}
+    if ( a == '^')
+	{
+		printf("Ответ: %lf", pow(x,y) );
+	}
+	return 0;
+}
