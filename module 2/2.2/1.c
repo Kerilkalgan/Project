16a43ff6a71aaa3b9b307f2b06cfed64fb9b6b95
Kerilkalgan <kerilkalgan@github.com>
2016-04-13 21:34:43 +0300

    13/04

--- module 2/2.2/1.c
+++ module 2/2.2/1.c
@@ -4,14 +4,13 @@ int main(void)
 	int days;
 	int year;
 	int mounth;
-    int day;
-    int x;
+    int day,d;
     printf("Введите количество дней, которые вы хотите конвертировать в года, месяцы и дни");
 	scanf("%d",&days);
 	year=days/365;
-    x=days-365*year;
-    mounth=x/30;
-	day=days-365*year-30*mounth;
+	d=days%365;
+    mounth=d/30.4;// Использую среднее значение кол-ва дней в месяце для того, чтобы результат был точнее.
+	day=days-365*year-30.4*mounth;
 	printf("Years= %d;\n",year);
 	printf("Mounths= %d;\n",mounth);
 	printf("Days=%d.\n",day);
--- module 2/2.2/2.c
+++ module 2/2.2/2.c
@@ -1,40 +1,54 @@
-#include <stdio.h>
-int main(void) 
+#include <stdio.h> 
+
+int main() 
+{ 
+    printf("Таблица ASCII\n\n");
+    printf("DEC  HEX  Simbolo\n");
+    printf(" 00  0      NULL\n");
+	printf(" 01  0x1    SOH\n");
+	printf(" 02  0x2    STX\n");
+	printf(" 03  0x3    ETX \n");
+	printf(" 04  0x4    EOT\n ");
+	printf("05  0x5    ENQ\n");
+	printf(" 06  0x6    ACK\n");
+	printf(" 07  0x7    BEL\n");   
+	printf(" 08  0x8    BS\n");     
+	printf(" 09  0x9    HL\n");
+	printf(" 10  0xa    LF\n");  
+	printf(" 12  0xb    FF\n");     
+	printf(" 13  0xd    CR\n");     
+	printf(" 14  0xe    SO\n");
+	printf(" 15  0xf    SI\n");
+	printf(" 16  0x10   DLE\n"); 
+	printf(" 17  0x11   DC1\n");    
+	printf(" 18  0x12   DC2\n");   
+	printf(" 19  0x13   DC3\n");   
+	printf(" 20  0x14   DC4\n");
+	printf(" 21  0x15   NAK\n");  
+	printf(" 22  0x16   SYN\n");
+	printf(" 23  0x17   ETB\n");  
+	printf(" 24  0x18   CAN\n");   
+	printf(" 25  0x19   EM\n");     
+	printf(" 26  0x1a   SAB\n"); 
+	printf(" 27  0x1b   ESC \n");
+	printf(" 28  0x1c   FS\n");
+	printf(" 29  0x1d   GS\n"); 
+	printf(" 30  0x1e   RS\n");
+	printf(" 31  0x1f   US\n");
+	printf(" 32  0x20   espacio\n");
+int i; 
+for (i=33;i<=126;i++)                   // Начинаю цикл для вывода с 33 символа по 126
 {
-	printf("Таблица ASCII\n");
-	printf("DEC  HEX  Simbolo   DEC  HEX  Simbolo   DEC HEX  Simbolo   DEX  HEX  Simbolo\n");
-	printf(" 00  00h    NULL     32  20h  espacio   64  40h     @       96  60h     `\n");
-	printf(" 01  01h    SOH      33  21h     !      65  41h     A       97  61h     a\n");
-	printf(" 02  02h    STX      34  22h     \"      66  42h     B       98  62h     b\n ");
-	printf("03  03h    ETX      35  23h     #      67  43h     C       99  63h     c\n");
-	printf(" 04  04h    EOT      36  24h     $      68  44h     D      100  64h     d\n");
-	printf(" 05  05h    ENQ     %s"," 37  25h     %      69  45h     E      101  65h     e\n");
-	printf(" 06  06h    ACK      38  26h     &      70  46h     F      102  66h     f\n");
-	printf(" 07  07h    BEL      39  27h     '      71  47h     G      103  67h     g\n");
-	printf(" 08  08h    BS       40  28h     (      72  48h     H      104  68h     h\n");
-	printf(" 09  09h    HL       41  29h     )      73  49h     I      105  69h     i\n");
-	printf(" 10  0Ah    LF       42  2Ah     *      74  4Ah     J      106  6Ah     j\n");
-	printf(" 11  0Bh    VT       43  2Bh     +      75  4Bh     K      107  6Bh     k\n");
-	printf(" 12  0Ch    FF       44  2Ch     ,      76  4Ch     L      108  6Ch     l\n");
-	printf(" 13  0Dh    CR       45  2Dh     -      77  4Dh     M      109  6Dh     m\n");
-	printf(" 14  0Eh    SO       46  2Eh     .      78  4Eh     N      110  6Eh     n\n");
-	printf(" 15  0Fh    SI       47  2Fh     /      79  4Fh     O      111  6Fh     o\n");
-	printf(" 16  10h    DLE      48  30h     0      80  50h     P      112  70h     p\n");
-	printf(" 17  11h    DC1      49  31h     1      81  51h     Q      113  71h     q\n");
-	printf(" 18  12h    DC2      50  32h     2      82  52h     R      114  72h     r\n");
-	printf(" 19  13h    DC3      51  33h     3      83  53h     S      115  73h     s\n");
-	printf(" 20  14h    DC4      52  34h     4      84  54h     T      116  74h     t\n");
-	printf(" 21  15h    NAK      53  35h     5      85  55h     U      117  75h     u\n");
-	printf(" 22  16h    SYN      54  36h     6      86  56h     V      118  76h     v\n");
-	printf(" 23  17h    ETB      55  37h     7      87  57h     W      119  77h     w\n");
-	printf(" 24  18h    CAN      56  38h     8      88  58h     X      120  78h     x\n");
-	printf(" 25  19h    EM       57  39h     9      89  59h     Y      121  79h     y\n");
-	printf(" 26  1Ah    SAB      58  3Ah     :      90  5Ah     Z      122  7Ah     z\n");
-	printf(" 27  1Bh    ESC      59  3Bh     ;      91  5Bh     [      123  7Bh     {\n");
-	printf(" 28  1Ch    FS       60  3Ch     <      92  5Ch     \\      124  7Ch     |\n");
-	printf(" 29  1Dh    GS       61  3Dh     =      93  5Dh     ]      125  7Dh     }\n");
-	printf(" 30  1Eh    RS       62  3Eh     >      94  5Eh     ^      126  7Eh     ~\n");
-	printf(" 31  1Fh    US       63  3Fh     ?      95  5Fh     _\n");
-	printf("127  20h    DEL");
-	return 0;
+    printf("%3d  %#x    %c \n",i,i,i); 
+} 
+    printf("127  0x7f   DEL");
+		
+return 0;
 }
+
+
+
+
+	
+
+
