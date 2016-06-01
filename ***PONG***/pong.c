d382bcd3de129a0aaf305871da2c864f979485c8
Kerilkalgan <kerilkalgan@github.com>
2016-06-01 13:59:26 +0300

    1/06

--- ***PONG***/pong.c
+++ ***PONG***/pong.c
@@ -1,9 +1,12 @@
 #include <allegro.h>
 #include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 
 int i=0,j=0;
+ char num_1[3];//для хранения очков
+ char num_2[3];
  
 int ball_x = 320; //начальные координаты шарика
 int ball_y = 240;
@@ -12,23 +15,23 @@ int ball_tempX = 320;
 int ball_tempY = 240;
 
 int p1_x = 20;//горизонтальная координата первой платформы
-int p1_y = 210;
+int p1_y = 210;//вертикальная координата
 
 int p1_tempX = 20;
 int p1_tempY = 210;
 
 int p2_x = 620;//горизонтальная координата второй платформы
-int p2_y = 210;
+int p2_y = 210;//вертикальная координата
 
 int p2_tempX = 620;
 int p2_tempY = 210;
 
-time_t secs;    //Секунды на системных часах будут храниться здесь это будет использоваться в качестве затравки для srand ()
+time_t secs;    //Секунды на системных часах 
 
 int dir;     // Направление шарика
              // при dir = 1 - вверх и влево, = 2 -вниз и влево, = 3 -вверх и вправо, = 4-вниз и вправо
          
-BITMAP *buffer; //Это будет наша временная битовая карта для двойной буферизации
+BITMAP *buffer; 
 
 
 void moveBall()//движение шара
@@ -74,7 +77,7 @@ void moveBall()//движение шара
                  ++ball_y;
          }
 
-    } else {//движение шарика после столкновения с объектом
+    } else {//движение шарика после столкновения с объектом (выбор дальнейшего пути)
 
         if (dir == 1 || dir == 3)    ++dir;
         else if (dir == 2 || dir == 4)    --dir;
@@ -82,10 +85,13 @@ void moveBall()//движение шара
     }
 
     acquire_screen();
-    circlefill ( buffer, ball_tempX, ball_tempY, 5, makecol( 100, 0, 0)); 
-    circlefill ( buffer, ball_x, ball_y, 5, makecol( 128, 255, 0)); // рисуем шарик с центром и радиус, цвет шарика  
+    circlefill ( buffer, ball_tempX, ball_tempY, 5, makecol( 30, 0, 0)); 
+    circlefill ( buffer, ball_x, ball_y, 5, makecol( 120, 55, 255)); // рисуем шарик с центром (ball_x,ball_y) и радиусом = 5, и указываем цвет шарика makecol (формат RGB)  
     draw_sprite( screen, buffer, 0, 0);
     release_screen();
+    textout_ex( screen, font, "***PONG***", 255, 20, makecol( 0, 0, 0), makecol( 255, 255, 255));
+    textout_ex( screen, font, num_1, 285, 65, makecol( 255, 255, 0), makecol( 0, 0, 0)); // вывод табло со счетом,очки первого
+    textout_ex( screen, font, num_2, 305, 65, makecol( 255, 255, 0), makecol( 0, 0, 0)); // очки второго
     rest(5);
 
 }
@@ -107,7 +113,7 @@ void p1Move(){ // управление платформой  1
 
     acquire_screen();
     rectfill( buffer, p1_tempX, p1_tempY, p1_tempX + 10, p1_tempY + 90, makecol ( 0, 0, 0));
-    rectfill( buffer, p1_x, p1_y, p1_x + 10, p1_y + 90, makecol ( 255, 255, 0)); //цвет первого, размеры
+    rectfill( buffer, p1_x, p1_y, p1_x + 10, p1_y + 90, makecol ( 255, 255, 10)); //цвет первой платформы,и её размеры,координаты
     release_screen();
 
 }
@@ -128,8 +134,7 @@ void p2Move(){ // управление платформой  2
 
     acquire_screen();
     rectfill( buffer, p2_tempX, p2_tempY, p2_tempX + 10, p2_tempY + 90, makecol ( 0, 0, 0));
-    rectfill( buffer, p2_x, p2_y, p2_x + 10, p2_y + 90, makecol ( 0, 255, 0));
-
+    rectfill( buffer, p2_x, p2_y, p2_x + 10, p2_y + 90, makecol ( 10, 255, 10));
     release_screen();
 
 }
@@ -141,6 +146,7 @@ void startNew()//начало новой игры
     clear_keybuf();
     readkey();
     clear_to_color( buffer, makecol( 0, 0, 0)); // цвет экрана
+    
     ball_x = 320;
     ball_y = 240;
 
@@ -157,31 +163,41 @@ void checkWin(){//Выявление победителя
  
     if ( ball_x < p1_x) 
     {
-        textout_ex( screen, font, "Player 2 Wins!", 240, 100, makecol( 255, 0, 0), makecol( 0, 0, 0));    
+        //textout_ex( screen, font, "Player 2 Wins!", 240, 50, makecol( 255, 255, 255), makecol( 0, 0, 0));
+     
         j++;        
         startNew();
         }
     if ( ball_x > p2_x)
     {
-        textout_ex( screen, font, "Player 1 Wins!", 240, 100, makecol( 255, 0, 0), makecol( 0, 0, 0));     
+        //textout_ex( screen, font, "Player 1 Wins!", 240, 50, makecol( 255, 255, 255), makecol( 0, 0, 0));
+      
         i++;
         startNew();
     }
+sprintf(num_1,"%d",i); //считывание очков в num_1 из i
+sprintf(num_2,"%d",j); //аналог. в num_2 из j
+
 }
 
-void setupGame(){//начальная прорисовка экрана
+void setupGame(){ //начальная прорисовка экрана
 
     acquire_screen();
     rectfill( buffer, p1_x, p1_y, p1_x + 10, p1_y + 90, makecol ( 255, 255, 0));
     rectfill( buffer, p2_x, p2_y, p2_x + 10, p2_y + 90, makecol ( 0, 255, 0));
-    circlefill ( buffer, ball_x, ball_y, 5, makecol( 128, 255, 0));
+    circlefill ( buffer, ball_x, ball_y, 5, makecol( 120, 55, 255));
    
     draw_sprite( screen, buffer, 0, 0);
     release_screen();
-
+    textout_ex( screen, font, "***PONG***", 255, 20, makecol( 0, 0, 0), makecol( 255, 255, 255));//начальная прорисовка названия игры
+    textout_ex( screen, font, "0", 285, 65, makecol( 255, 255, 0), makecol( 0, 0, 0)); // далее нулевого табло очков
+    textout_ex( screen, font, "0", 305, 65, makecol( 255, 255, 0), makecol( 0, 0, 0));
     time(&secs);
     srand( (unsigned int)secs);
-    dir = rand() % 4 + 1;
+    
+    readkey(); //игра начнется только при нажатии клавиш 
+ 
+    dir = rand() % 4 + 1; // здесь генирируется одно из четырех направлений движения в самом начале игры
 
 }
 
@@ -197,19 +213,18 @@ int main(){
 
     setupGame();
 
-    while( !key[KEY_ESC] && i<15 && j<15) {  //ESC для выхода из игры
-		  
-       
-		   while(key[KEY_P]); // при удерживании клавиши P будет происходить пауза
+    while( !key[KEY_ESC] && i<15 && j<15)   //Цикл игры, который можно прервать нажатив клавишу ESC 
+		                                     //или при достижении одного из игроков 15 очков игра прекращается
+    {   
+		   while(key[KEY_P]);  // При нажатии клавиши P и удержании её игра будет остановлена
 			
-
         p1Move();
         p2Move();
         moveBall();
         checkWin();
 
     }
-printf("Финальный счет %d : %d",i,j);
+    printf("Финальный счет %d : %d",i,j);
     return 0;
 
 }
